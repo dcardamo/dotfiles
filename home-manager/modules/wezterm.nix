049aaa99b23c62a43f0ca74f776c7082ef7a9605
Dan Cardamore <dan@hld.ca>
2024-09-05 20:33:49 -0400

    .

--- home-manager/modules/wezterm.nix
+++ home-manager/modules/wezterm.nix
@@ -6,32 +6,6 @@ in {
   home.packages = [ (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; }) ];
   programs.wezterm = {
     enable = true;
-    colorSchemes.onedarkpro = {
-      background = "#000000";
-      foreground = "#abb2bf";
-      selection_bg = "#d55fde";
-      selection_fg = "#abb2bf";
-      ansi = [
-        "#000000"
-        "#ef596f"
-        "#89ca78"
-        "#e5c07b"
-        "#61afef"
-        "#d55fde"
-        "#2bbac5"
-        "#abb2bf"
-      ];
-      brights = [
-        "#434852"
-        "#f38897"
-        "#a9d89d"
-        "#edd4a6"
-        "#8fc6f4"
-        "#e089e7"
-        "#4bced8"
-        "#c8cdd5"
-      ];
-    };
     extraConfig = ''
       local w = require('wezterm')
       local config = w.config_builder()
@@ -46,7 +20,7 @@ in {
         config.window_decorations = 'RESIZE'
       end
 
-      config.color_scheme = 'onedarkpro'
+      config.color_scheme = 'Tokyo Night Moon'
       config.cursor_blink_rate = 0
       config.font = w.font_with_fallback({"MonoLisa", "FiraCode Nerd Font"})
       config.font_size = 13
