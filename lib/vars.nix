11b8d8d6623a9e7486be51f64beb7c5e94004147
Dan Cardamore <dan@hld.ca>
2024-10-25 01:07:14 +0000

    adding ipad mini key

--- lib/vars.nix
+++ lib/vars.nix
@@ -13,9 +13,14 @@
     ''
       ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJ0uue7ICe3WJiXuKbFlsO9kZY+Az6TCDrn67Tl/KReHJPq4V86XdihWCG08IFUqSFzBqIC8zw6rmVJ+rduTYDU= dan@ipad
     ''
+
     # mac laptop mars
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFe+4bVpPWZTF344M5TRzaz5/90s5finWdYFXhs+mwac dan@dans-mbp.lan"
+
     # dan iphone 15 pro
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEaCACN0Pby6uZWYBZ3umr8SCobH6OQgQ5gYs7IQUM55kiTY0A+l5HJ7FYKNUcYXq+HPbwkZ33ixjkfaZc99OTU= dan@iphone"
+
+    # dan ipad Mini
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJMhMLaN7oNq528VmycRtdj2y/iO5Ug1zo1YHH39upSSZ2TbJzqc7Aqf+5zCyO3z5udrEA8pOEovYYE3JMteaRE= dan@ipadmini"
   ];
 }
