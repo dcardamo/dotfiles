cf21ac2ab9121b8d5ed92512eaad7fb177f406f1
Dan Cardamore <dan@hld.ca>
2024-12-09 09:15:45 -0500

    adding orbstack to path

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -106,6 +106,7 @@ in {
       + lib.strings.optionalString isDarwin ''
         fish_add_path /opt/homebrew/bin
         fish_add_path ~/.local/bin
+        fish_add_path ~/.orbstack/bin
       '';
 
     functions = {
