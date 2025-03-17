fa9bdfe348fe9007a3fcb670176c4627365c1c43
Dan Cardamore <dan@hld.ca>
2025-03-17 06:29:38 -0600

    temporary filelist.txt

--- bin/sync-videos-to-processed.sh
+++ bin/sync-videos-to-processed.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Set DRYRUN to 1 for a dry run, 0 for actual execution
-DRYRUN=0
+DRYRUN=1
 
 # Set source and destination directories
 SRC=~/Sync/Photos/originals/
 DEST=~/Sync/Photos/published/
 
+# Create a temporary file for the file list
+FILELIST=$(mktemp)
+
 # Generate a list of files with case-insensitive extensions, relative to $SRC
 cd "$SRC" || exit 1  # Change to source directory, exit if failed
-find . -type f | awk 'tolower($0) ~ /\.(mov|mp4|mpg|avi|mkv)$/ {print substr($0, 3)}' > filelist.txt
+find . -type f | awk 'tolower($0) ~ /\.(mov|mp4|mpg|avi|mkv)$/ {print substr($0, 3)}' > "$FILELIST"
 
 # Construct the rsync command
-RSYNC_CMD="rsync -av --progress --info=stats2 --files-from=filelist.txt \"$SRC\" \"$DEST\""
+RSYNC_CMD="rsync -av --progress --info=stats2 --files-from=\"$FILELIST\" \"$SRC\" \"$DEST\""
 
 # Append --dry-run if DRYRUN is enabled
 if [ "$DRYRUN" -eq 1 ]; then
@@ -24,3 +27,6 @@ fi
 
 # Run the command
 eval $RSYNC_CMD
+
+# Clean up: Remove the temporary file
+rm -f "$FILELIST"
