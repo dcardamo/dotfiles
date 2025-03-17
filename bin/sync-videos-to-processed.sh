f009099c22bf65972b80dfd4d2d85267de7f6bd0
Dan Cardamore <dan@hld.ca>
2025-03-17 06:28:50 -0600

    improve sync

--- bin/sync-videos-to-processed.sh
+++ bin/sync-videos-to-processed.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-# Copy all movie files from originals to processed using rsync
-rsync -av \
-   --progress --info=stats2 \
-   --include='*/' \
-   --include='*.mov' \
-   --include='*.mp4' \
-   --include='*.mpg' \
-   --include='*.avi' \
-   --include='*.mkv' \
-   --exclude='*' \
-   ~/Sync/Photos/originals/ \
-   ~/Sync/Photos/published/
+# Set DRYRUN to 1 for a dry run, 0 for actual execution
+DRYRUN=0
+
+# Set source and destination directories
+SRC=~/Sync/Photos/originals/
+DEST=~/Sync/Photos/published/
+
+# Generate a list of files with case-insensitive extensions, relative to $SRC
+cd "$SRC" || exit 1  # Change to source directory, exit if failed
+find . -type f | awk 'tolower($0) ~ /\.(mov|mp4|mpg|avi|mkv)$/ {print substr($0, 3)}' > filelist.txt
+
+# Construct the rsync command
+RSYNC_CMD="rsync -av --progress --info=stats2 --files-from=filelist.txt \"$SRC\" \"$DEST\""
+
+# Append --dry-run if DRYRUN is enabled
+if [ "$DRYRUN" -eq 1 ]; then
+    RSYNC_CMD="$RSYNC_CMD --dry-run"
+    echo "Performing a dry run..."
+else
+    echo "Executing file transfer..."
+fi
+
+# Run the command
+eval $RSYNC_CMD
