3c52aa2f0eec40c14cc1f8cbc434d38f9a79f829
Dan Cardamore <dan@hld.ca>
2025-07-03 07:38:34 -0400

    small improvements

--- home-manager/modules/neovim/lua/plugins/telescope.lua
+++ home-manager/modules/neovim/lua/plugins/telescope.lua
@@ -104,8 +104,8 @@ return {
           },
           mappings = {
             i = {
-              ["<C-n>"] = actions.cycle_history_next,
-              ["<C-p>"] = actions.cycle_history_prev,
+              ["<C-n>"] = actions.move_selection_next,
+              ["<C-p>"] = actions.move_selection_previous,
               ["<C-j>"] = actions.move_selection_next,
               ["<C-k>"] = actions.move_selection_previous,
               ["<C-c>"] = actions.close,
